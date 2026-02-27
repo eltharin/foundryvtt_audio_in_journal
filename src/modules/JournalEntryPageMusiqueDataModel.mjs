525eeee725d3bc6b9140dc30d17fb839d844210c
eltharin <eltharin18@hotmail.fr>
2026-02-27 15:21:53 +0100

    first commit

---
+++ src/modules/JournalEntryPageMusiqueDataModel.mjs
@@ -0,0 +1,16 @@
+
+
+
+export class JournalEntryPageMusiqueDataModel extends foundry.abstract.TypeDataModel {
+  static defineSchema() {
+    const fields = foundry.data.fields;
+    return {
+    //  src: new fields.FilePathField({required: false, categories: ["AUDIO"]}),
+      
+    };
+  }
+
+  prepareDerivedData() {
+
+  }
+}
